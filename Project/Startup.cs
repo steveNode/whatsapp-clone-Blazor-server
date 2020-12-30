60fdf3c238f450b95d65755a0cd18ada277454be
steveNode <70070832+steveNode@users.noreply.github.com>
2020-12-30 17:45:34 +0530

    Update Startup.cs

--- Project/Startup.cs
+++ Project/Startup.cs
@@ -15,7 +15,9 @@ namespace Project
     {
         public Startup(IConfiguration configuration)
         {
+            
             Configuration = configuration;
+            
         }
 
         public IConfiguration Configuration { get; }
